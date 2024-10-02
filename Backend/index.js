cef2fc359d2ac15646b0c4e6228ae59d627c3186
Nikita <nikkyapandey@gmail.com>
2024-10-02 23:22:31 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,7 @@ import { config } from 'dotenv';
 config();  // Load environment variables
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.get('/colleges', (req, res) => {
     const colleges = [
